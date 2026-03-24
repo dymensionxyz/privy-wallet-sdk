fa492977a40c60d7b0537525bcf6c3aa92524480
Michael Tsitrin <michael@dymension.xyz>
2026-03-24 12:25:12 +0200

    fixed example page

--- example/test-page/vite.config.ts
+++ example/test-page/vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      'react',
+      'react-dom',
+      '@privy-io/react-auth',
+      '@privy-io/wagmi',
+      '@tanstack/react-query',
+      'wagmi',
+      'viem',
+    ],
+  },
   server: { port: 5174 },
   define: {
     global: 'globalThis',
